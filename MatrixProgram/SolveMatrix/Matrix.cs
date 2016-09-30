5efa39a087762a69dd631629e64c3717cd52c574
PalinaBerazavikova <Palina_Berazavikova@epam.com>
2016-09-30 14:20:30 +0300

    1

--- MatrixProgram/SolveMatrix/Matrix.cs
+++ MatrixProgram/SolveMatrix/Matrix.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
-
+using System.IO;
 
 namespace SolveMatrix
 {
@@ -22,56 +22,73 @@ namespace SolveMatrix
         {
             this.Path = ConfigurationManager.AppSettings["path"];
         }
-        //public void ReadFromFileSecond()
-        //{
-        //    while ((line = file.ReadLine()) != "")
-        //    {
-        //        continue;
-        //    }
-        //    while ((line = file.ReadLine()) != null)
-        //    {
-        //        lines.Add(line.Trim());
-        //        Console.WriteLine(line);
-        //    }
-        //    file.Close();
-        //    string[] numbers = lines[0].Split(' ');
-        //    int rows = lines.Count, columns = numbers.Length;
-        //    this.K = rows;
-        //    this.ResultMatrixCols = columns;
+        public void ReadFromFileSecond()
+        {
+            List<string> lines = new List<string>();
+            if (File.Exists(this.Path))
+            {
+                string line;                
+                System.IO.StreamReader file = new System.IO.StreamReader(this.Path);
+                while ((line = file.ReadLine()) != "")
+                {
+                    continue;
+                }
+                while ((line = file.ReadLine()) != null)
+                {
+                    lines.Add(line.Trim());
+                    Console.WriteLine(line);
+                }
+                file.Close();
+            }
+            else
+            {
+                Console.WriteLine("wrong path!");
 
-        //    this.SecondMatrix = new double[rows, columns];
-        //    int j = 0;
-        //    int i = 0;
-        //    foreach (var row in lines)
-        //    {
-        //        j = 0;
-        //        foreach (var col in row.Trim().Split(' '))
-        //        {
-        //            this.SecondMatrix[i, j] = double.Parse(col.Trim());
-        //            j++;
-        //        }
-        //        i++;
-        //    }
+            }
+            
+            string[] numbers = lines[0].Split(' ');
+            int rows = lines.Count, columns = numbers.Length;
+            this.K = rows;
+            this.ResultMatrixCols = columns;
+
+            this.SecondMatrix = new double[rows, columns];
+            int j = 0;
+            int i = 0;
+            foreach (var row in lines)
+            {
+                j = 0;
+                foreach (var col in row.Trim().Split(' '))
+                {
+                    this.SecondMatrix[i, j] = double.Parse(col.Trim());
+                    j++;
+                }
+                i++;
+            }
+        }
 
 
-        //}
+
         public void ReadFromFileFirst()
         {
             List<string> lines = new List<string>();
-            string line, line2;
-            System.IO.StreamReader file = new System.IO.StreamReader(this.Path);
-            while ((line = file.ReadLine()) != "" )
+            
+            if (File.Exists(this.Path))
             {
-                lines.Add(line.Trim());
-                Console.WriteLine(line);  
+                string line;
+                System.IO.StreamReader file = new System.IO.StreamReader(this.Path);
+                while ((line = file.ReadLine()) != "")
+                {
+                    lines.Add(line.Trim());
+                    Console.WriteLine(line);
+                }
+
+                file.Close();
             }
-            Console.WriteLine("111111111111111111111");
-            while ((line2 = file.ReadLine()) != "")
+            else
             {
-                lines.Add(line.Trim());
-                Console.WriteLine(line2);
-            }//почему цикл?
-            file.Close();
+                Console.WriteLine("wrong path!");
+               
+            }
             string[] numbers = lines[0].Split(' ');
             int rows = lines.Count, columns = numbers.Length;
             this.ResultMatrixRows = rows;
